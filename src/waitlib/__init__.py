dac0a4c2d2e416c7a95ec23a7006843587b20e89
Hai Vu <haivu2004@gmail.com>
2025-11-11 11:06:31 -0800

    refactor: Remove cli.py

--- src/waitlib/__init__.py
+++ src/waitlib/__init__.py
@@ -1,4 +1,3 @@
-from .cli import main
 from .waitlib import wait_for_async
 
-__all__ = ["main", "wait_for_async"]
+__all__ = ["wait_for_async"]
--- src/waitlib/cli.py
+++
@@ -1,7 +0,0 @@
-import click
-
-
-@click.command()
-@click.version_option()
-def main() -> None:
-    print("Hello world!")
